7575b3f3b9f751755c79c65b76ec675f8bc88ed9
stevengangano <stevengangano@yahoo.com>
2018-03-23 15:35:14 -0700

    completed notes for photo app

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,6 +2,7 @@ class Image < ActiveRecord::Base
   belongs_to :user
   mount_uploader :picture, PictureUploader
   validate :picture_size
+  validates_presence_of :picture
 
   private
   def picture_size
